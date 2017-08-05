7cebb52fc85e23cbcc645bc9306616a27c3889a1
JJ <jjohnson@ernestsports.com>
2017-08-05 13:42:33 -0400

    Initial Commit

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by  on 8/5/17.
+//  Copyright © 2017 Ernest Sports. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
